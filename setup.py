d66fae710a0a4b55ad8f2c8f4043c076dbbddba3
Danniel Hugo <danniel.ribeiro@gerencianet.com.br>
2017-01-26 14:20:56 -0200

    Update version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ dependencies = [dependency.strip()
 setup(
     name='gerencianet',
 
-    version='1.0.6',
+    version='1.0.7',
 
     description='Module for integration with Gerencianet API',
     long_description=long_description,
